954d172661108391cd2ae3596d4eaa317e13d0b2
NiyaWi <Niyaw144@gmail.com>
2023-11-01 01:07:32 -0400

    partial page layout

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//it is used to calculate out how much a monthly payment for a loan would be (minus any insurance or taxes)//
+//as well as how much interest you would pay over the life of the loan//
+
+//loan amount - insaurance and taxes
+//monthly payment
+//interest
+//time of loan
+
+let monthlyPayment = 
+let
+
+function monthlyPayment(){
+let  
+
+}
